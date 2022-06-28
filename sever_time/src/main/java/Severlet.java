650bbf7aeb8669dd158e4bc05a6591f6129ba896
unknown <you@example.com>
2022-06-28 16:55:57 +0700

    th1

---
+++ sever_time/src/main/java/Severlet.java
@@ -0,0 +1,32 @@
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DatabaseMetaData;
+import java.util.Date;
+
+@WebServlet(urlPatterns = "/index")
+
+ public class Severlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<html>");
+
+        Date date = new Date();
+
+        printWriter.println("<h1>" + date + " </h1>");
+        printWriter.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+}
